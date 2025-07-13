610d959077002496aa3b893d734f0e00754ac27a
adriana quinteor <adriana.quintero@g2i.co>
2025-07-13 18:25:46 -0500

    files

--- main.go
+++ main.go
@@ -1,18 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	parts := 3
 
-	total := parallelSum(numbers, parts)
-	fmt.Println("Total sum:", total)
-}
-
-// parallelSum splits the slice and sums in parallel
-func parallelSum(nums []int, workers int) int {
-	return 0
 }
